53a8732cac54cd65b35286e667dec74135a7f167
eroatta <emilianoroatta@gmail.com>
2019-11-30 20:23:01 -0300

    added raw logic for frequency table creation use case

--- usecase/create_freqtable.go
+++ usecase/create_freqtable.go
@@ -2,34 +2,53 @@ package usecase
 
 import "github.com/eroatta/freqtable/entity"
 
-type FrequencyTableUseCase interface {
-	Create() (entity.FrequencyTable, error)
-	Merge() (entity.FrequencyTable, error)
+import "context"
+
+import "github.com/eroatta/freqtable/repository"
+
+// CreateFrequencyTableUsecase defines the contract for the use cases related to the
+// creation of one or several frenquency tables.
+type CreateFrequencyTableUsecase interface {
+	// Create creates a single frequency table.
+	Create(ctx context.Context, url string) (entity.FrequencyTable, error)
+	// CreateMultiple creates several frequency tables.
+	CreateMultiple(ctx context.Context, urls []string) ([]entity.FrequencyTable, error)
 }
 
-type asd struct {
+// NewCreateFrequencyTableUsecase initializes a new CreateFrequencyTableUsecase handler
+// with the given repositories.
+func NewCreateFrequencyTableUsecase(wcr repository.WordCountRepository, ftr repository.FrequencyTableRepository) createFrequencyTableUsecase {
+	return createFrequencyTableUsecase{
+		wcr: wcr,
+		ftr: ftr,
+	}
 }
 
-/*
-ft := FrequencyTable{
-		ID: "",
+type createFrequencyTableUsecase struct {
+	wcr repository.WordCountRepository
+	ftr repository.FrequencyTableRepository
+}
+
+// Create creates a new entity.FrequencyTable from the given URL.
+func (uc createFrequencyTableUsecase) Create(ctx context.Context, url string) (entity.FrequencyTable, error) {
+	ft := entity.FrequencyTable{
+		ID: "", // TODO: build ID from URL
 	}
 
-	values, err := fts.wcr.Extract(url)
+	values, err := uc.wcr.Extract(url)
 	if err != nil {
 		// TODO: log
 		ft.Error = err
-		return ft
+		return ft, err // TODO: should we send and error on this cases?
 	}
 	ft.Values = values
 
-	context := context.Background() // this is empty right now
-	err = fts.ftr.Save(context, ft)
+	err = uc.ftr.Save(ctx, ft)
 	if err != nil {
 		// TODO: log
 		ft.Error = err
-		return ft
+		return ft, err
 	}
 
-	return ft
-*/
+	return ft, nil
+}
---
+++ usecase/create_freqtable_test.go
@@ -0,0 +1,17 @@
+package usecase_test
+
+import "testing"
+
+import "github.com/stretchr/testify/assert"
+
+func TestNewCreateFrequencyTableUsecase_ShouldReturnNewInstance(t *testing.T) {
+	assert.FailNow(t, "not yet implemented")
+}
+
+func TestCreate_OnCreateFrequencyTableUsecase_ShouldCreateFrequencyTable(t *testing.T) {
+	assert.FailNow(t, "not yet implemented")
+}
+
+func TestCreate_OnCreateFrequencyTableUsecase_WhenErrorCounting_ShouldReturnError(t *testing.T) {
+	assert.FailNow(t, "not yet implemented")
+}
