b2f068394cab1b6333048c9b4617cfe526a37a04
eroatta <emilianoroatta@gmail.com>
2019-12-15 19:43:30 -0300

    removed dead code

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	url := "https://github.com/src-d/go-siva"
 	processor := wordcount.NewProcessor(config)
-	frequencies, err := processor.Extract(url) //Build(url, config)
+	frequencies, err := processor.Extract(url)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -30,32 +30,3 @@ func main() {
 		log.Println(fmt.Sprintf("Repository: %s - Word: %s - Count: %d", url, token, count))
 	}
 }
-
-/*
-type BuilderConfig struct {
-	cloner wordcount.Cloner
-	miner  wordcount.Miner
-}*/
-
-/*
-var ErrCloningRepository = errors.New("Error while reading/cloning remote repository")
-
-func Build(url string, config BuilderConfig) (map[string]int, error) {
-	// cloning step
-	_, filesc, err := wordcount.Clone(url, config.cloner)
-	if err != nil {
-		// TODO: improve error logging
-		log.Println(fmt.Sprintf("Error reading repository %s: %v", url, err))
-		return nil, ErrCloningRepository
-	}
-
-	// parsing step
-	parsedc := wordcount.Parse(filesc)
-	files := wordcount.Merge(parsedc)
-
-	// mining step
-	miningResults := wordcount.Mine(files, config.miner)
-	countResults := miningResults.(miner.Count)
-
-	return countResults.Results().(map[string]int), nil
-}*/
