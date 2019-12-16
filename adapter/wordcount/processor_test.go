0102ec742eb8821d9fbc141dbb40b7737f507f89
eroatta <emilianoroatta@gmail.com>
2019-12-16 20:03:02 -0300

    added tests for processor

--- adapter/wordcount/processor_test.go
+++ adapter/wordcount/processor_test.go
@@ -1,22 +1,118 @@
 package wordcount_test
 
 import (
-	"github.com/stretchr/testify/assert"
+	"errors"
+	"go/ast"
 	"testing"
+
+	"github.com/eroatta/freqtable/adapter/wordcount"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewProcessor_ShouldReturnNewProcessor(t *testing.T) {
-	assert.FailNow(t, "not yet implemented")
+	processor := wordcount.NewProcessor(wordcount.ProcessorConfig{})
+
+	assert.NotNil(t, processor)
 }
 
 func TestExtract_OnProcessorWithFailingCloningStep_ShouldReturnError(t *testing.T) {
-	assert.FailNow(t, "not yet implemented")
+	cloner := testCloner{
+		err: errors.New("HTTP 404 Not Found"),
+	}
+
+	config := wordcount.ProcessorConfig{
+		Cloner: cloner,
+		Miner:  nil,
+	}
+	processor := wordcount.NewProcessor(config)
+	_, err := processor.Extract("https://github.com/eroatta/freqtable")
+
+	assert.EqualError(t, err, wordcount.ErrCloningRepository.Error())
 }
 
 func TestExtract_OnProcessorWithFailingParsingStep_ShouldReturnError(t *testing.T) {
-	assert.FailNow(t, "not yet implemented")
+	cloner := testCloner{
+		repository: wordcount.Repository{
+			Name: "freqtable",
+			URL:  "https://github.com/eroatta/freqtable",
+		},
+		filenames: []string{"main.go"},
+		file:      []byte("packaaaage main"),
+	}
+
+	config := wordcount.ProcessorConfig{
+		Cloner: cloner,
+		Miner:  nil,
+	}
+	processor := wordcount.NewProcessor(config)
+	_, err := processor.Extract("https://github.com/eroatta/freqtable")
+
+	assert.EqualError(t, err, wordcount.ErrParsingFile.Error())
 }
 
 func TestExtract_OnProcessor_ShouldReturnValidResults(t *testing.T) {
-	assert.FailNow(t, "not yet implemented")
+	cloner := testCloner{
+		repository: wordcount.Repository{
+			Name: "freqtable",
+			URL:  "https://github.com/eroatta/freqtable",
+		},
+		filenames: []string{"main.go"},
+		file:      []byte("package main"),
+	}
+
+	miner := testMiner{
+		results: map[string]int{
+			"main": 1,
+		},
+	}
+
+	config := wordcount.ProcessorConfig{
+		Cloner: cloner,
+		Miner:  miner,
+	}
+	processor := wordcount.NewProcessor(config)
+	results, err := processor.Extract("https://github.com/eroatta/freqtable")
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(results))
+	assert.Equal(t, 1, results["main"])
+}
+
+type testCloner struct {
+	repository wordcount.Repository
+	filenames  []string
+	file       []byte
+	err        error
+}
+
+func (t testCloner) Clone(url string) (wordcount.Repository, error) {
+	if t.err != nil {
+		return wordcount.Repository{}, t.err
+	}
+
+	return t.repository, nil
+}
+
+func (t testCloner) Filenames() ([]string, error) {
+	return t.filenames, nil
+}
+
+func (t testCloner) File(name string) ([]byte, error) {
+	return t.file, nil
+}
+
+type testMiner struct {
+	results map[string]int
+}
+
+func (t testMiner) Name() string {
+	return "testMiner"
+}
+
+func (t testMiner) Visit(node ast.Node) ast.Visitor {
+	return t
+}
+
+func (t testMiner) Results() map[string]int {
+	return t.results
 }
